7ec506c57bcce4455eb988df2281df82578d02cd
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2025-10-05 00:03:50 +0800

    point download prebuilt binary link to my fork

--- lua/fff/download.lua
+++ lua/fff/download.lua
@@ -2,7 +2,7 @@ local M = {}
 local system = require('fff.utils.system')
 local uv = vim and vim.uv or require('luv')
 
-local GITHUB_REPO = 'dmtrKovalenko/fff.nvim'
+local GITHUB_REPO = 'madmaxieee/fff.nvim'
 
 local function get_current_version(plugin_dir, callback)
   vim.system({ 'git', 'rev-parse', '--short', 'HEAD' }, { cwd = plugin_dir }, function(result)
